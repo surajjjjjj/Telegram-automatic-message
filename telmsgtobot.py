6303f6c7567b53d6af04f1dd8aaf8be4b91fc93f
surajjjjjj <38719027+surajjjjjj@users.noreply.github.com>
2019-02-14 12:59:50 +0530

    Update telmsgtobot.py

--- telmsgtobot.py
+++ telmsgtobot.py
@@ -4,7 +4,7 @@ import time
 #generate api id and hash on https://my.telegram.org
 api_id = #enter api id
 api_hash = #enter api hash
-client = TelegramClient('session_name', api_id, api_hash)
+client = TelegramClient('userbot', api_id, api_hash)
 client.start()
 BCbot = client.get_entity('BTC_Faucet_freebot')
 count=0
